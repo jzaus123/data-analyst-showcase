1a0b74655a42b85f712ebe18a4d73cdca65c3abe
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-12 21:49:57 +1100

    Create data_cleaning.py

---
+++ data_cleaning.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+def clean_price_demand_dataset(df_price_demand):
+    #convert data types to datetime
+    df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'], format='%d/%m/%Y %H:%M', errors='ignore')
+
+    #Step 4: Deal with missing data
+    price_missing_num = 0
+    for colunm_name in df_price_demand.columns:
+        price_missing_num = df_price_demand[colunm_name].isnull().sum()
+
+    #drop off NaN values
+    pd.set_option('display.max_rows', 500)
+    df_price_demand.dropna(axis= 0, inplace=True)
+    df_price_demand.isnull().values.any()
+
+    return df_price_demand
+  
+def clean_weather_dataset(df_weather):
+
+    #convert data types to datetime
+    df_weather['Date'] = pd.to_datetime(df_weather['Date'], format='%d/%m/%Y', errors='ignore')
+
+    #convert object to float 
+    df_weather['9am wind speed (km/h)'] = pd.to_numeric(df_weather['9am wind speed (km/h)'], errors='coerce')
+    df_weather['3pm wind speed (km/h)'] = pd.to_numeric(df_weather['3pm wind speed (km/h)'], errors= 'coerce')
+
+    #Step 4: Deal with missing data
+    total_missing_num = 0
+    for colunm_name in df_weather.columns:
+        weather_missing_num = df_weather[colunm_name].isnull().sum()
+        total_missing_num += weather_missing_num
+
+    #drop off NaN values
+    pd.set_option('display.max_rows', 500)
+    df_weather.dropna(axis= 0, inplace=True)
+
+    #check NaN value 
+    df_weather.isnull().values.any()
+
+    # Step 5: Filter out data outliers
+    #use IQR (Inter Quartile Range)-IQR = Quartile3 – Quartile1
+    for colunm_name in df_weather.columns:
+        if df_weather[colunm_name].dtypes == 'object' or colunm_name == 'Date':
+            continue
+        Q1 = np.percentile(df_weather[colunm_name], 25, method = 'midpoint')
+        
+        Q3 = np.percentile(df_weather[colunm_name], 75, method = 'midpoint')
+
+        IQR = Q3 - Q1      
+
+        upper_bound = Q3 + 1.5 * IQR
+        lower_bound = Q1 - 1.5 * IQR
+
+        df_upper_bound = df_weather.loc[df_weather[colunm_name] > upper_bound]
+        df_lower_bound = df_weather.loc[df_weather[colunm_name] < lower_bound]
+    
+    return df_weather
