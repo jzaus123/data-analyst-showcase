6414024cec9f0f273582ac0579c40d2505dd792a
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-12 21:55:28 +1100

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,9 +11,25 @@ def clean_price_demand_dataset(df_price_demand):
         price_missing_num = df_price_demand[colunm_name].isnull().sum()
 
     #drop off NaN values
-    pd.set_option('display.max_rows', 500)
     df_price_demand.dropna(axis= 0, inplace=True)
     df_price_demand.isnull().values.any()
+
+    # Step 5: Filter out data outliers
+    #use IQR (Inter Quartile Range)-IQR = Quartile3 – Quartile1
+    for colunm_name in df_price_demand.columns:
+        if df_price_demand[colunm_name].dtypes == 'object' or colunm_name == 'SETTLEMENTDATE':
+            continue
+        Q1 = np.percentile(df_price_demand[colunm_name], 5, method = 'midpoint')
+        
+        Q3 = np.percentile(df_price_demand[colunm_name], 95, method = 'midpoint')
+
+        IQR = Q3 - Q1      
+
+        upper_bound = Q3 + 1.5 * IQR
+        lower_bound = Q1 - 1.5 * IQR
+
+        df_upper_bound = df_price_demand.loc[df_price_demand[colunm_name] < upper_bound]
+        df_lower_bound = df_price_demand.loc[df_price_demand[colunm_name] > lower_bound]
 
     return df_price_demand
   
@@ -33,7 +49,6 @@ def clean_weather_dataset(df_weather):
         total_missing_num += weather_missing_num
 
     #drop off NaN values
-    pd.set_option('display.max_rows', 500)
     df_weather.dropna(axis= 0, inplace=True)
 
     #check NaN value 
@@ -44,9 +59,9 @@ def clean_weather_dataset(df_weather):
     for colunm_name in df_weather.columns:
         if df_weather[colunm_name].dtypes == 'object' or colunm_name == 'Date':
             continue
-        Q1 = np.percentile(df_weather[colunm_name], 25, method = 'midpoint')
+        Q1 = np.percentile(df_weather[colunm_name], 5, method = 'midpoint')
         
-        Q3 = np.percentile(df_weather[colunm_name], 75, method = 'midpoint')
+        Q3 = np.percentile(df_weather[colunm_name], 95, method = 'midpoint')
 
         IQR = Q3 - Q1      
 
