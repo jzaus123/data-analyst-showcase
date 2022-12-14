5fb2e67900fc248c20c5efb88b533ec08a2deb7b
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-14 22:55:16 +1100

    replace remove missing function

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import numpy as np
 
+def remove_missing_value(df):
+
+   #check amount of missing value in each colunm and decide if missing value is dropped off or filled in aggregated values
+    for colunm_name in df.columns:
+        total_missing_count = df[colunm_name].isnull().sum()
+
+        if total_missing_count != 0:
+            print(f'{colunm_name} has {total_missing_count} missing values')
+
+    #here we just chose to drop off missing values 
+    #in other scenarios we can also choose to fill in aggregated values such as mean, median or mode
+    df.dropna(axis = 0, inplace = True) 
+
 def clean_price_demand_dataset(df_price_demand):
     
     df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'], format='%d/%m/%Y %H:%M', errors='ignore')
 
-    #check amount of NaN value in each colunm and decide if NaN value is dropped off or filled in aggregated values
-    for colunm_name in df_price_demand.columns:
-        price_missing_num = df_price_demand[colunm_name].isnull().sum()
-
-        if price_missing_num != 0:
-            print(f'{colunm_name} has {price_missing_num} NaN')
-
-    #based on above check there is a small amount of NaN values so we can just drop them off
-    df_price_demand.dropna(axis = 0, inplace = True)
+    remove_missing_value(df_price_demand)
 
     # TODO Deduplicate your data - no duplicated data by checking via df.duplicated()
 
@@ -47,16 +52,6 @@ def clean_weather_dataset(df_weather):
     df_weather['9am wind speed (km/h)'] = pd.to_numeric(df_weather['9am wind speed (km/h)'], errors='coerce')
     df_weather['3pm wind speed (km/h)'] = pd.to_numeric(df_weather['3pm wind speed (km/h)'], errors= 'coerce')
 
-    #Deal with missing data
-    total_missing_num = 0
-    for colunm_name in df_weather.columns:
-        weather_missing_num = df_weather[colunm_name].isnull().sum()
-        total_missing_num += weather_missing_num
-
-    #drop off NaN values
-    df_weather.dropna(axis= 0, inplace=True)
-
-    #check NaN value 
-    df_weather.isnull().values.any()
+    remove_missing_value(df_weather)
 
     return df_weather
