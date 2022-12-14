1266dfd46dc8adf408253b928e891872f482420d
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-14 22:34:00 +1100

    remove commonts

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,19 +2,18 @@ import pandas as pd
 import numpy as np
 
 def clean_price_demand_dataset(df_price_demand):
-    #convert data types to datetime
+    
     df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'], format='%d/%m/%Y %H:%M', errors='ignore')
 
-    #Deal with missing data
+    #check amount of NaN value in each colunm and decide if NaN value is dropped off or filled in aggregated values
     for colunm_name in df_price_demand.columns:
         price_missing_num = df_price_demand[colunm_name].isnull().sum()
 
         if price_missing_num != 0:
             print(f'{colunm_name} has {price_missing_num} NaN')
 
-    #drop off NaN values
-    df_price_demand.dropna(axis= 0, inplace=True)
-    df_price_demand.isnull().values.any()
+    #based on above check there is a small amount of NaN values so we can just drop them off
+    df_price_demand.dropna(axis = 0, inplace = True)
 
     #Filter out data outliers
     #use IQR (Inter Quartile Range)-IQR = Quartile3 – Quartile1
@@ -22,7 +21,6 @@ def clean_price_demand_dataset(df_price_demand):
         if df_price_demand[colunm_name].dtypes == 'object' or colunm_name == 'SETTLEMENTDATE':
             continue
         Q1 = np.percentile(df_price_demand[colunm_name], 5, method = 'midpoint')
-        
         Q3 = np.percentile(df_price_demand[colunm_name], 95, method = 'midpoint')
 
         IQR = Q3 - Q1      
@@ -33,8 +31,8 @@ def clean_price_demand_dataset(df_price_demand):
         df_upper_bound = df_price_demand.loc[df_price_demand[colunm_name] > upper_bound]
         df_lower_bound = df_price_demand.loc[df_price_demand[colunm_name] < lower_bound]
 
-        df_price_demand.drop(df_upper_bound, inplace=True, errors = 'ignore')
-        df_price_demand.drop(df_lower_bound, inplace=True,errors = 'ignore')
+        df_price_demand.drop(df_upper_bound, inplace = True, errors = 'ignore')
+        df_price_demand.drop(df_lower_bound, inplace = True,errors = 'ignore')
 
     return df_price_demand
   
