02c5d1c3c9c3dc89716f72fc6d17fc1d2784227e
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-14 23:14:22 +1100

    Define drop_outlier_data function

--- data_cleaning.py
+++ data_cleaning.py
@@ -12,46 +12,53 @@ def remove_missing_value(df):
 
     #here we just chose to drop off missing values 
     #in other scenarios we can also choose to fill in aggregated values such as mean, median or mode
-    df.dropna(axis = 0, inplace = True) 
+    df.dropna(axis = 0, inplace = True)
 
-def clean_price_demand_dataset(df_price_demand):
-    
-    df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'], format='%d/%m/%Y %H:%M', errors='ignore')
-
-    remove_missing_value(df_price_demand)
-
-    # TODO Deduplicate your data - no duplicated data by checking via df.duplicated()
-
+def drop_outlier_data(df, ignored_column_list):
     #Filter out data outliers
     #use IQR (Inter Quartile Range)-IQR = Quartile3 – Quartile1
-    for colunm_name in df_price_demand.columns:
-        if df_price_demand[colunm_name].dtypes == 'object' or colunm_name == 'SETTLEMENTDATE':
+    for colunm_name in df.columns:
+        if df[colunm_name].dtypes == 'object' or colunm_name in ignored_column_list:
             continue
-        Q1 = np.percentile(df_price_demand[colunm_name], 5, method = 'midpoint')
-        Q3 = np.percentile(df_price_demand[colunm_name], 95, method = 'midpoint')
+        Q1 = np.percentile(df[colunm_name], 5, method = 'midpoint')
+        Q3 = np.percentile(df[colunm_name], 95, method = 'midpoint')
 
         IQR = Q3 - Q1      
 
         upper_bound = Q3 + 1.5 * IQR
         lower_bound = Q1 - 1.5 * IQR
 
-        df_upper_bound = df_price_demand.loc[df_price_demand[colunm_name] > upper_bound]
-        df_lower_bound = df_price_demand.loc[df_price_demand[colunm_name] < lower_bound]
+        df_upper_bound = df.loc[df[colunm_name] > upper_bound]
+        df_lower_bound = df.loc[df[colunm_name] < lower_bound]
 
-        df_price_demand.drop(df_upper_bound, inplace = True, errors = 'ignore')
-        df_price_demand.drop(df_lower_bound, inplace = True, errors = 'ignore')
+        df.drop(df_upper_bound, inplace = True, errors = 'ignore')
+        df.drop(df_lower_bound, inplace = True, errors = 'ignore')
+
+def clean_price_demand_dataset(df_price_demand):
+    
+    df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'], format='%d/%m/%Y %H:%M', errors='coerce')
+
+    remove_missing_value(df_price_demand)
+
+    # TODO Deduplicate your data - no duplicated data by checking via df.duplicated()
+
+    drop_outlier_data(df_price_demand, ['SETTLEMENTDATE'])
 
     return df_price_demand
   
 def clean_weather_dataset(df_weather):
 
     #convert data types to datetime
-    df_weather['Date'] = pd.to_datetime(df_weather['Date'], format='%d/%m/%Y', errors='ignore')
+    df_weather['Date'] = pd.to_datetime(df_weather['Date'], format='%d/%m/%Y', errors='coerce')
 
     #convert object to float 
     df_weather['9am wind speed (km/h)'] = pd.to_numeric(df_weather['9am wind speed (km/h)'], errors='coerce')
     df_weather['3pm wind speed (km/h)'] = pd.to_numeric(df_weather['3pm wind speed (km/h)'], errors= 'coerce')
 
     remove_missing_value(df_weather)
+
+    # TODO deduplicate
+    
+    drop_outlier_data(df_weather, ['Date'])
 
     return df_weather
