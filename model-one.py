d3f2f01ecebe444b33606582ec77687d91ebde8f
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-15 22:20:00 +1100

    looping colunm

--- model-one.py
+++ model-one.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import data_cleaning
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 pd.set_option('display.max_rows', 500) 
 
@@ -13,6 +16,12 @@ df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDA
 df_daily_totaldemand = df_price_demand.groupby(['SETTLEMENTDATE']).sum()
 
 df_weather_daily_totaldemand_joined = pd.merge(df_weather, df_daily_totaldemand, how = 'inner', left_on = 'Date', right_on = 'SETTLEMENTDATE')
-print(df_weather_daily_totaldemand_joined.shape)
+print(df_weather_daily_totaldemand_joined.head())
 
-
+for start_colunm_index in range(1, 21):
+    feature_list = []
+    for colunm_name in df_weather_daily_totaldemand_joined.columns[start_colunm_index:]:
+        
+        feature_list.append(colunm_name)
+        print(feature_list)
+ 
