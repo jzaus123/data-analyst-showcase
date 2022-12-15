64ca2d05e3c719e8ea12800b616c7ce74708d240
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-15 16:23:11 +1100

    grougby daily

--- model-one.py
+++ model-one.py
@@ -1,3 +1,16 @@
 import pandas as pd
-import numpy as np
+import data_cleaning
+
+pd.set_option('display.max_rows', 500) 
+
+df_price_demand = pd.read_csv('price_demand_data.csv')
+df_weather = pd.read_csv('weather_data.csv')
+
+data_cleaning.clean_price_demand_dataset(df_price_demand)
+data_cleaning.clean_weather_dataset(df_weather)
+
+df_daily_totaldemand = df_price_demand['TOTALDEMAND'].groupby(df_price_demand['SETTLEMENTDATE'].dt.to_period('D')).sum()
+
+df_weather_daily_totaldemand_joined = pd.merge(df_weather, df_daily_totaldemand, how = 'outer', left_index=True, right_index=True)
+
 
