e3a92912ef394e6f1fc76fd25c4e89aaa2192537
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-15 21:07:16 +1100

    update groupby

--- model-one.py
+++ model-one.py
@@ -9,8 +9,10 @@ df_weather = pd.read_csv('weather_data.csv')
 data_cleaning.clean_price_demand_dataset(df_price_demand)
 data_cleaning.clean_weather_dataset(df_weather)
 
-df_daily_totaldemand = df_price_demand['TOTALDEMAND'].groupby(df_price_demand['SETTLEMENTDATE'].dt.to_period('D')).sum()
+df_price_demand['SETTLEMENTDATE'] = pd.to_datetime(df_price_demand['SETTLEMENTDATE'].dt.date)
+df_daily_totaldemand = df_price_demand.groupby(['SETTLEMENTDATE']).sum()
 
-df_weather_daily_totaldemand_joined = pd.merge(df_weather, df_daily_totaldemand, how = 'outer', left_index=True, right_index=True)
+df_weather_daily_totaldemand_joined = pd.merge(df_weather, df_daily_totaldemand, how = 'inner', left_on = 'Date', right_on = 'SETTLEMENTDATE')
+print(df_weather_daily_totaldemand_joined.shape)
 
 
