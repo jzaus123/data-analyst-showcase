9f3c7eec769b2bc8537a94342dd90f0c879339a0
jzaus123 <114069805+jzaus123@users.noreply.github.com>
2022-12-17 12:52:19 +1100

    fixed typo

--- model-one.py
+++ model-one.py
@@ -20,44 +20,44 @@ df_daily_totaldemand = df_price_demand.groupby(['SETTLEMENTDATE']).sum()
 
 df_weather_daily_totaldemand_joined = pd.merge(df_weather, df_daily_totaldemand, how = 'inner', left_on = 'Date', right_on = 'SETTLEMENTDATE')
 
-#convert object variables to numeric variables in order to calculate Pearson's coorelation coefficient
-#features selection by appling Pearson's coorelation coefficient
-pearson_coorelation_dict = {}
-for colunm_name in df_weather_daily_totaldemand_joined.columns:
-    if colunm_name in ['TOTALDEMAND', 'Date']:
+#convert object variables to numeric variables in order to calculate Pearson's correlation coefficient
+#feature selection by appling Pearson's correlation coefficient
+pearson_correlation_dict = {}
+for column_name in df_weather_daily_totaldemand_joined.columns:
+    if column_name in ['TOTALDEMAND', 'Date']:
         continue
-    elif df_weather_daily_totaldemand_joined[colunm_name].dtypes == 'object':
-        df_weather_daily_totaldemand_joined[colunm_name] = pd.factorize(df_weather_daily_totaldemand_joined[colunm_name])[0] 
-        pearson_corr = df_weather_daily_totaldemand_joined[colunm_name].corr(df_weather_daily_totaldemand_joined['TOTALDEMAND'])
-        pearson_coorelation_dict[colunm_name] = abs(pearson_corr)
+    elif df_weather_daily_totaldemand_joined[column_name].dtypes == 'object':
+        df_weather_daily_totaldemand_joined[column_name] = pd.factorize(df_weather_daily_totaldemand_joined[column_name])[0] 
+        pearson_corr = df_weather_daily_totaldemand_joined[column_name].corr(df_weather_daily_totaldemand_joined['TOTALDEMAND'])
+        pearson_correlation_dict[column_name] = abs(pearson_corr)
     
     else:
-        pearson_corr = df_weather_daily_totaldemand_joined[colunm_name].corr(df_weather_daily_totaldemand_joined['TOTALDEMAND'])
-        pearson_coorelation_dict[colunm_name] = abs(pearson_corr)       
+        pearson_corr = df_weather_daily_totaldemand_joined[column_name].corr(df_weather_daily_totaldemand_joined['TOTALDEMAND'])
+        pearson_correlation_dict[column_name] = abs(pearson_corr)       
 
-#find out features with highest Pearson coorealtion coefficient
+#find out features with highest Pearson correaltion coefficient
 selected_feature_count = 6
-features_selection = sorted(pearson_coorelation_dict.items(), key = lambda x:x[1], reverse = True)[:selected_feature_count]
+feature_selection = sorted(pearson_correlation_dict.items(), key = lambda x:x[1], reverse = True)[:selected_feature_count]
 
-features_selection = [feature_name[0] for feature_name in features_selection]
+feature_selection = [feature_name[0] for feature_name in feature_selection]
 best_r2_test_score = 0
 best_feature_list = []
-for feature_index in range(0, len(features_selection)):
-    features_list = []
-    for features in features_selection[feature_index:]:
-        features_list.append(features)
+for feature_index in range(0, len(feature_selection)):
+    feature_list = []
+    for feature in feature_selection[feature_index:]:
+        feature_list.append(feature)
 
-        feature_data = df_weather_daily_totaldemand_joined[features_list]
+        feature_data = df_weather_daily_totaldemand_joined[feature_list]
         target_data = df_weather_daily_totaldemand_joined['TOTALDEMAND']
         features_train, features_test, target_train, target_test = train_test_split(feature_data, target_data, test_size=0.2, random_state=42)
 
-        #modelling - lienar regression
+        #modelling - linear regression
         lm = linear_model.LinearRegression()
         model = lm.fit(features_train, target_train)
         r2_test_score = lm.score(features_test, target_test)
 
         if r2_test_score > best_r2_test_score:
             best_r2_test_score = r2_test_score
-            best_feature_list = copy.deepcopy(features_list)
+            best_feature_list = copy.deepcopy(feature_list)
 
 print(f'Best r2 test score {best_r2_test_score} with features combination {best_feature_list}')
